073b086e2f84c79a8246ea831dd98db6cf9b0e87
SamusMoran <kimberlym4488@comcast.net>
2022-01-12 19:47:12 -0800

    Updates classes to correct names and super functionality

--- index.js
+++ index.js
@@ -2,27 +2,42 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require ('jest')
 const HTMLtemplate = require("./dist/generateHTML")
-const classes = require("./lib/classes")
+const Employee= require("./lib/employee")
+const Manager = require('./lib/manager')
+const Engineer = require('./lib/engineer')
+const Intern = require('./lib/engineer')
+const questions = require('./lib/questions')
 
- async function init() {
-     
-    inquirer.prompt(prompts);
 
-    try{
-        const name = await inquirer.prompt(getName())
-        //Handle response data
-        const id = await inquirer.prompt(getID());
+//push to list of employees
+function askForManagerInfo() {
+    inquirer
+    .prompt()
+    .then((answers) => {
+        //create and store an object for the manager
+        employees.push( new Manager(//pass in data from inquirer);
+        //ask what they would like to do next
+        askforwhattodonext()
+        ))
+    })
+}
 
-        const email = await inquirer.prompt(getEmail());
+//loop over stored employees to build an html page that includes all of their information. 
 
-    }
-    catch (err) {
-        //Handle Error
-    }
 
+async function askForManagerInfo() {
+
+    const answers = await inquirer.prompt(/* questions */ )
+        //create and store an object for the manager
+        employees.push( new Manager(//pass in data from inquirer);
+        //ask what they would like to do next
+        askForNextAction()
+        ))
+}
+
+function askForNextAction() {
+    askForManagerInfo();
 }
 
 // When you're done
 prompts.complete();
-
-init();
---
+++ test/employee.test.js
@@ -0,0 +1,68 @@
+const Employee = require("../lib/employee")
+
+//positive tests - testing object properties after things have been constructed. Should throw an error ifprovided no arguments. 
+//should create object with a name and age.
+//describe each function and return values you are getting for each employee, engineer, manager, intern
+
+describe("Employee", () => {
+
+    //Methods
+    describe("Initialization", () => {
+
+    //testing of the object properties after constructing a new instance
+        it ("should build with these properties", () =>{
+            
+    })
+        
+    })
+    //Methods
+    describe("getName", () => {
+        it ("should build with these properties", () =>{
+
+        })
+        
+        
+    })
+    describe("getName", () => {
+        it ("should build with these properties", () =>{
+            
+        })
+        //Arrange
+        //Create a new Employee object
+
+        //Act
+        //Call the getName method and store the result
+
+        //Assert
+        //The result is equal to an expected value.
+        
+
+    })
+    describe("getName", () => {
+           it ("should build with these properties", () =>{
+            
+        })
+        
+
+    })
+    describe("getName", () => {
+        it ("should build with these properties", () =>{
+            
+        })
+
+
+    })
+    describe("getName", () => {
+        it ("should build with these properties", () =>{
+            
+        })
+
+    })
+    describe("getName", () => {
+        it ("should build with these properties", () =>{
+            
+        })
+
+    })
+    
+}
