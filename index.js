9a08a6aa61a4a8f62d3ae226c5aa5647e4ee94bc
unknown <kimberlym4488@comcast.net>
2022-01-20 20:57:04 -0800

    All validation complete for inquirer prompts

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const jest = require ('jest')
+const jest = require('jest')
 const HTMLtemplate = require("./src/generateHTML")
 const Employee = require("./lib/employee")
 const Manager = require('./lib/manager')
@@ -16,14 +16,14 @@ const employees = [];
 async function askForManagerInfo() {
 
     try {
-         
-    const answers = await inquirer
-    .prompt
-    (questionsMan)
-      const manager = new Manager(answers.name, answers.id, answers.email, answers.phoneNumber)
+
+        const answers = await inquirer
+            .prompt
+            (questionsMan)
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.phoneNumber)
         console.log(answers)
         //create and store an object for the manager
-        employees.push( manager//pass in data from inquirer);
+        employees.push(manager//pass in data from inquirer);
         )
         //ask what they would like to do next
         menu();
@@ -37,32 +37,33 @@ async function askForManagerInfo() {
 async function menu() {
 
     try {
-         
-    const answers = await inquirer
-    .prompt
-    ([
-        {type: "list",
-         name: "next",
-         message: "Who should we add next?",
-         choices: ["Engineer", "Intern", "I'm done"]
-    }
-    ])
 
-        if (answers.next === "Engineer"){
+        const answers = await inquirer
+            .prompt
+            ([
+                {
+                    type: "list",
+                    name: "next",
+                    message: "Who should we add next?",
+                    choices: ["Engineer", "Intern", "I'm done"]
+                }
+            ])
+
+        if (answers.next === "Engineer") {
             askForEngineerInfo();
         }
 
         //create and store an object for the manager
-        else if (answers.next === "Intern"){
+        else if (answers.next === "Intern") {
             askForInternInfo();
         }
-        
-        else{
+
+        else {
 
             fs.writeFile('./dist/team.HTML', HTMLtemplate(employees),
-        (err) =>
-        err ? console.error(err) : console.log('Success! Your team.html page is now ready to be viewed!'))
-        //pass in data from inquirer);
+                (err) =>
+                    err ? console.error(err) : console.log('Success! Your team.html page is now ready to be viewed!'))
+            //pass in data from inquirer);
         }
     }
     catch (error) {
@@ -73,14 +74,15 @@ async function menu() {
 async function askForEngineerInfo() {
 
     try {
-         
-    const answers = await inquirer
-    .prompt
-    (questionsEng)
-         const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+
+        const answers = await inquirer
+            .prompt
+            (questionsEng)
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
         //create and store an object for the manager
+        console.log(answers)
         employees.push(engineer)//pass in data from inquirer);
-        
+
         //ask what they would like to do next
         menu();
     }
@@ -93,11 +95,11 @@ async function askForEngineerInfo() {
 async function askForInternInfo() {
 
     try {
-         
-    const answers = await inquirer
-    .prompt
-    (questionsIntern)
-         const intern= new Intern(answers.name, answers.id, answers.email, answers.school)
+
+        const answers = await inquirer
+            .prompt
+            (questionsIntern)
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
         //create and store an object for the manager
         employees.push(intern)//pass in data from inquirer);
         //ask what they would like to do next
